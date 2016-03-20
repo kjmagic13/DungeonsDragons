06f289ae99b97146dc1c6209e61af0ae4a3d7393
Kevin McCarthy <kjmagic13@gmail.com>
2016-03-20 14:11:30 -0400

    init player creation

--- app/models/concerns/player_creation.rb
+++ app/models/concerns/player_creation.rb
@@ -10,11 +10,11 @@ module PlayerCreation
 			if true # standard
 				standard_assignments = [15,14,13,12,10,8]
 				ABILITIES.each_with_index do |ability, index|
-					player.send("#{ability.underscore}=", standard_assignments[index])
+					player.send "#{ability.underscore}=", standard_assignments[index]
 				end
 			else # roll
 				ABILITIES.each do |ability|
-					player.send("#{ability.underscore}=", best_x_of_yDz)
+					player.send "#{ability.underscore}=", best_x_of_yDz(3,4,6)
 				end
 			end
 
@@ -26,7 +26,7 @@ module PlayerCreation
 
 		private
 
-		def best_x_of_yDz(x=3, y=4, z=6)
+		def best_x_of_yDz(x=1, y=1, z=20)
 			throws = []
 			y.times do
 				throws << rand(1..z)
